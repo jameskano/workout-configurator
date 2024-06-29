79a70fe6fb96d6d8781810e88b0c890ba33d3820
jameskano <jaimecanovasdesign@gmail.com>
2024-06-29 15:27:53 +0200

    Sidenav icons and style

--- client/src/components/side-navbar/SideNavBar.tsx
+++ client/src/components/side-navbar/SideNavBar.tsx
@@ -1,8 +1,13 @@
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
-import "./SideNavBar.scss";
-import { SideNavbarTypes } from "./SideNavbar.types";
-import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './SideNavBar.scss';
+import { SideNavbarTypes } from './SideNavbar.types';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import FitnessCenterRoundedIcon from '@mui/icons-material/FitnessCenterRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import ContentPasteRoundedIcon from '@mui/icons-material/ContentPasteRounded';
+// import EqualizerRoundedIcon from '@mui/icons-material/EqualizerRounded';
+// import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 
 const SideNavBar = ({ showSideNavbar, setIsSideNavbarShown }: SideNavbarTypes) => {
 	const logOutHandler = () => {};
@@ -10,24 +15,30 @@ const SideNavBar = ({ showSideNavbar, setIsSideNavbarShown }: SideNavbarTypes) =
 	const sideNavbarHandler = () => setIsSideNavbarShown(false);
 
 	return (
-		<section className={`side-navbar side-navbar--${showSideNavbar ? "show" : "hide"}`}>
-			<div className="side-navbar__option">
-				<NavLink
-					to="/exercises"
-					onClick={sideNavbarHandler}
-					className={({ isActive }) => (isActive ? "active" : undefined)}>
-					Exercises
-					<ArrowForwardIosRoundedIcon />
-				</NavLink>
+		<section className={`side-navbar side-navbar--${showSideNavbar ? 'show' : 'hide'}`}>
+			<div className='side-navbar__option'>
+				<FitnessCenterRoundedIcon />
+				<div>
+					<NavLink
+						to='/exercises'
+						onClick={sideNavbarHandler}
+						className={({ isActive }) => (isActive ? 'active' : undefined)}>
+						Exercises
+						<ArrowForwardIosRoundedIcon />
+					</NavLink>
+				</div>
 			</div>
-			<div className="side-navbar__option">
-				<NavLink
-					to="/workouts"
-					onClick={sideNavbarHandler}
-					className={({ isActive }) => (isActive ? "active" : undefined)}>
-					Workouts
-					<ArrowForwardIosRoundedIcon />
-				</NavLink>
+			<div className='side-navbar__option'>
+				<ContentPasteRoundedIcon />
+				<div>
+					<NavLink
+						to='/workouts'
+						onClick={sideNavbarHandler}
+						className={({ isActive }) => (isActive ? 'active' : undefined)}>
+						Workouts
+						<ArrowForwardIosRoundedIcon />
+					</NavLink>
+				</div>
 			</div>
 			{/* <div className="side-navbar__option">
                 <NavLink
@@ -43,21 +54,24 @@ const SideNavBar = ({ showSideNavbar, setIsSideNavbarShown }: SideNavbarTypes) =
             </div> */}
 			{/* <div className="side-navbar__option">
                 <NavLink
-                    to="/account"
+                    to="/configuration" (account, theme, etc)
                     onClick={sideNavbarHandler}
                     className={({ isActive }) =>
                         isActive ? "active" : undefined
                     }
                 >
-                    <span>Account</span>
+                    <span>Configuration</span>
                     <ArrowForwardIosRoundedIcon />
                 </NavLink>
             </div> */}
-			<div className="side-navbar__option">
-				<Link to="/" onClick={logOutHandler}>
-					Log out
-					<ArrowForwardIosRoundedIcon />
-				</Link>
+			<div className='side-navbar__option'>
+				<LogoutRoundedIcon />
+				<div>
+					<Link to='/' onClick={logOutHandler}>
+						Log out
+						<ArrowForwardIosRoundedIcon />
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
