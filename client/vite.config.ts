5ffd43ba14d7230f8b96731b08703e946aaac6cb
jameskano <jaimecanovasdesign@gmail.com>
2024-08-08 14:05:10 +0200

    Update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000
-        port: 3000,
-    },
+	plugins: [react()],
+	server: {
+		// this ensures that the browser opens upon server start
+		open: true,
+		// this sets a default port to 3000
+		port: 3000,
+	},
+	build: {
+		rollupOptions: {
+			external: ['react', 'react-router', 'react-router-dom'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+			},
+		},
+	},
 });
