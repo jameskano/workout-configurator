c5952b898fdf2b604d24d0a286da093dd98d7f18
jameskano <jaimecanovasdesign@gmail.com>
2024-08-08 14:17:18 +0200

    Change vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,14 +10,4 @@ export default defineConfig({
 		// this sets a default port to 3000
 		port: 3000,
 	},
-	build: {
-		rollupOptions: {
-			external: ['react', 'react-router', 'react-router-dom'],
-			output: {
-				globals: {
-					react: 'React',
-				},
-			},
-		},
-	},
 });
